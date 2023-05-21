55a7199e9d4ef1e53aeb0516a1eb8cd70ec94944
Abel Hiram <hiram9629@outlook.com>
2023-05-21 14:33:27 -0700

    checks to wait for db

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,10 +2,12 @@
 calculator functions
 """
 
-def add(x,y):
+
+def add(x, y):
     """add x  + y ,return result"""
     return x+y
 
-def substract(x,y):
+
+def substract(x, y):
     """substract x  + y ,return result"""
     return y-x
