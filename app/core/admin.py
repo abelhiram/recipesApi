747544ee16556b08063c2722cd4288c46a41fe8b
abelhiram <hiram9629@outlook.com>
2023-05-22 18:28:11 -0700

    user model

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,13 @@
-from django.contrib import admin  # noqa
+"""django admin ustomization"""
 
-# Register your models here.
+from django.contrib import admin  # noqa
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """define the admin pages for users"""
+    order = ['id']
+    list_display = ['email','name']
+
